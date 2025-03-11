772749ac73ec709e4fb8aac7aa6ca5ec140a2741
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-03-11 15:26:58 -0500

    Assign initial DOM objetcs to variables

---
+++ main.js
@@ -0,0 +1,5 @@
+const display = document.querySelector('.display');
+const number = document.querySelector('.number');
+const operator = document.querySelector('.operator');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
