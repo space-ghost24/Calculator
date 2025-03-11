c7c60c7f0c90b9be7c36fa3a7b6dc286c2c063c5
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-03-11 16:35:37 -0500

    Add operate function and event listeners for numbers

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 const display = document.querySelector('.display');
-const number = document.querySelector('.number');
-const operator = document.querySelector('.operator');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equals');
 const clear = document.querySelector('.clear');
+
+let num1 = '';
+let num2 = '';
+let op = '';
+let result;
+
+function operate(op, num1, num2){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    switch(op){
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if (num2 === 0){
+                return 'you died'
+            }else{
+                return num1 / num2;
+            };
+    };
+};
+
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        let value = e.target.textContent;
+
+        if(op === ''){
+            num1 += value;
+            display.textContent = num1;
+        }else{
+            num2 += value;
+            display.textContent = num2;
+        }
+    });
+});
